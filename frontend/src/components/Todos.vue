e24a8d68fa730c51d69946daed1b72c4b1b8c370
Eugene Ivantsov <eivantsov@redhat.com>
2018-03-27 20:03:39 +0300

    Update Todos.vue

--- frontend/src/components/Todos.vue
+++ frontend/src/components/Todos.vue
@@ -34,7 +34,7 @@
           >
         </div>
         <div class="col-sm-2 text-right">
-          <button type="submit" class="btn btn-primary" @click="addTask" >Add now</button>
+          <button type="submit" class="btn btn-primary" @click="addTask" >Add a ToDo</button>
         </div>
       </div>
 
