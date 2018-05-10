fb696c88105c186d3a39c6b5fceac72c91eb4651
Ilya Buziuk <ilyabuziuk@gmail.com>
2018-05-10 13:39:35 +0000

    Fixing login problem

--- users-api/src/test/java/com/elgris/usersapi/UsersApiApplicationTests.java
+++ users-api/src/test/java/com/elgris/usersapi/UsersApiApplicationTests.java
@@ -19,10 +19,22 @@ import com.elgris.usersapi.models.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsersApiApplicationTests {
-    
+    @Autowired
+    UsersController controller;
+
     @Test
-    public void dummyTest() {
-        assertEquals(true, true);
+    public void adminLogin() {
+        String username = "admin";
+        String firstname = "Foo";
+        
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Claims claims = mock(Claims.class);
+        
+        when(request.getAttribute("claims")).thenReturn(claims);
+        when(claims.get("username")).thenReturn(username);
+        
+        User user = controller.getUser(request, username);
+        assertEquals(user.getFirstname(), firstname);
     }
 
 }
