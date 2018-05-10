e61b50b792ea1eb8a4e69baedbac8a8df816cc5e
Ilya Buziuk <ibuziuk@redhat.com>
2018-05-10 03:07:53 +0200

    Reverting PR with fix
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- users-api/src/test/java/com/elgris/usersapi/UsersApiApplicationTests.java
+++ users-api/src/test/java/com/elgris/usersapi/UsersApiApplicationTests.java
@@ -1,8 +1,8 @@
 package com.elgris.usersapi;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertEquals;
 import io.jsonwebtoken.Claims;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,11 +19,24 @@ import com.elgris.usersapi.models.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsersApiApplicationTests {
+    
+    @Autowired
+    UsersController controller;
 
 
     @Test
-    public void defaultTest() {
-        assertEquals(true, true);
+    public void adminLogin() {
+        String username = "admin";
+        String firstname = "Foo";
+        
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Claims claims = mock(Claims.class);
+        
+        when(request.getAttribute("claims")).thenReturn(claims);
+        when(claims.get("username")).thenReturn(username);
+        
+        User user = controller.getUser(request, username);
+        assertEquals(user.getFirstname(), firstname);
     }
 
 }
